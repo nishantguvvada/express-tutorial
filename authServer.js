3db76fefc48802896076cd293c3543a4dd28df00
Nishant Guvvada <nishant.guvvada@gmail.com>
2025-12-08 10:37:23 +0530

    added token endpoint in auth server

--- authServer.js
+++ authServer.js
@@ -13,6 +13,26 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 
+let refreshTokens = [];
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.sendStatus(204);
+});
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+
+    if (refreshToken == null) return res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(401);
+        const accessToken = generateAccessToken({ name: user.name });
+        res.json({ accessToken: accessToken })
+    });
+});
+
 app.post('/login', (req, res) => {
     // authenticate the username first
     const username = req.body.username;
@@ -21,6 +41,7 @@ app.post('/login', (req, res) => {
     const user = { name: username }
     const accessToken = generateAccessToken(user);
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken);
     res.json({accessToken: accessToken, refreshToken: refreshToken});
 
 });
